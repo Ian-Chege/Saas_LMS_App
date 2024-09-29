5ab0447ea69bbf5ba6b717bd14b26917a88f4c84
Ian Chege <ianchege001@gmail.com>
2024-09-29 19:06:02 +0300

    refactor: Update landing page navbar and menu components
    Refactor the landing page navbar and menu components to improve code readability and maintainability.
    - Replace the `LogInIcon` component with the `MenuIcon` component from the `lucide-react` library.
    - Add a new `GlassSheet` component to create a transparent overlay for the mobile menu.
    - Update the styling and layout of the navbar and menu components for better responsiveness.
    
    Related to #issue-number

--- src/app/(landing)/_components/navbar/index.tsx
+++ src/app/(landing)/_components/navbar/index.tsx
@@ -1,25 +1,39 @@
-import { LogInIcon } from "lucide-react"
+import { MenuIcon } from "lucide-react"
 import Link from "next/link"
 
+import GlassSheet from "@/components/global/glass-sheet"
+import { Button } from "@/components/ui/button"
+import { Logout } from "@/icons/logout"
 import { Menu } from "./menu"
 
 export const LandingPageNavbar = () => {
-    return (
-        <div className="w-full flex justify-between items-center py-4 sticky top-0 z-50">
-            {/* Application Name */}
-            <p className="text-xl font-bold">Qwertz</p>
-            {/* Menu style - desktop/mobile format */}
-            <Menu orientation="desktop" />
-            <div className="flex border rounded-md gap-x-6 px-4 py-2">
-                <div>Home</div>
-                <div>Features</div>
-                <div>Pricing</div>
-            </div>
-
-            <Link href="/sign-in" className="flex items-center">
-                <LogInIcon className="w-6 h-6" />
-                <span className="ml-2">Login</span>
-            </Link>
-        </div>
-    )
+  return (
+    <div className="w-full flex justify-between items-center py-4 sticky top-0 z-50">
+      {/* Application Name */}
+      <p className="text-xl font-bold">Qwertz</p>
+      {/* Menu style - desktop/mobile format */}
+      <Menu orientation="desktop" />
+      <div className="flex gap-2">
+        <Link href="/sign-in">
+          <Button
+            variant="outline"
+            className="bg-themeBlack rounded-2xl flex gap-2 border-themeGray hover:bg-themeGray"
+          >
+            <Logout />
+            Login
+          </Button>
+        </Link>
+        <GlassSheet
+          triggerClass="lg:hidden"
+          trigger={
+            <Button variant="ghost" className="hover:bg-transparent">
+              <MenuIcon size={30} />
+            </Button>
+          }
+        >
+          <Menu orientation="mobile" />
+        </GlassSheet>
+      </div>
+    </div>
+  )
 }
--- src/app/(landing)/_components/navbar/menu.tsx
+++ src/app/(landing)/_components/navbar/menu.tsx
@@ -1,19 +1,64 @@
 "use client"
 
+import Link from "next/link"
+
+import { Card, CardContent } from "@/components/ui/card"
+import { GROUPLE_CONSTANTS } from "@/constants"
 import { useNavigation } from "@/hooks/navigation"
+import { cn } from "@/lib/utils"
 
 interface MenuProps {
-    orientation: "desktop" | "mobile"
+  orientation: "desktop" | "mobile"
 }
 
 export const Menu = ({ orientation }: MenuProps) => {
-    const { section, onSetSection } = useNavigation()
-    switch (orientation) {
-        case "desktop":
-            return <div className="flex gap-x-4"></div>
-        case "mobile":
-            return <div className="flex gap-x-4"></div>
-        default:
-            return null
-    }
+  const { section, onSetSection } = useNavigation()
+  switch (orientation) {
+    case "desktop":
+      return (
+        <Card className="bg-themeGray border-themeGray bg-clip-padding backdrop--blur__safari backdrop-filter backdrop-blur-2xl bg-opacity-60 p-1 lg:flex hidden rounded-xl">
+          <CardContent className="p-0 flex gap-2">
+            {GROUPLE_CONSTANTS.landingPageMenu.map((menuItem: any) => (
+              <Link
+                key={menuItem.id}
+                href={menuItem.path}
+                {...(menuItem.section && {
+                  onClick: () => onSetSection(menuItem.path),
+                })}
+                className={cn(
+                  "rounded-xl flex gap-2 py-2 px-4 items-center",
+                  section == menuItem.path && "bg-[#09090B] border-[#27272A]",
+                )}
+              >
+                {section == menuItem.path && menuItem.icon}
+                {menuItem.label}
+              </Link>
+            ))}
+          </CardContent>
+        </Card>
+      )
+    case "mobile":
+      return (
+        <div className="flex flex-col mt-10">
+          {GROUPLE_CONSTANTS.landingPageMenu.map((menuItem) => (
+            <Link
+              href={menuItem.path}
+              {...(menuItem.section && {
+                onClick: () => onSetSection(menuItem.path),
+              })}
+              className={cn(
+                "rounded-xl flex gap-2 py-2 px-4 items-center",
+                section == menuItem.path ? "bg-themeGray border-[#27272A]" : "",
+              )}
+              key={menuItem.id}
+            >
+              {menuItem.icon}
+              {menuItem.label}
+            </Link>
+          ))}
+        </div>
+      )
+    default:
+      return null
+  }
 }
