171957f352301fae34da0109aaaeb29366b2547e
Ian-Chege <ianchegedev@gmail.com>
2024-10-14 16:01:43 +0300

    feat: action retrieval

---
+++ src/app/group/[groupId]/layout.tsx
@@ -0,0 +1,48 @@
+import { onAuthenticatedUser } from "@/actions/auth"
+import { onGetAllGroupMembers, onGetGroupChannels, onGetGroupInfo, onGetGroupSubscriptions, onGetUserGroups } from "@/actions/groups"
+import { QueryClient } from "@tanstack/react-query"
+import { redirect } from "next/navigation"
+
+type GroupLayoutProps = {
+    children: React.ReactNode
+    params: {
+        groupId: string
+    }
+}
+
+const GroupLayout = async({children, params}: GroupLayoutProps) => {
+    const query = new QueryClient()
+    const user = await onAuthenticatedUser()
+    if(!user.id) redirect('sign-in')
+
+    // group info
+    await query.prefetchQuery({
+        queryKey: ['group-info'],
+        queryFn: () => onGetGroupInfo(params.groupId)
+    })
+
+    // user groups
+    await query.prefetchQuery({
+        queryKey: ['user-groups'],
+        queryFn: () => onGetUserGroups(user.id)
+    })
+    // channels
+    await query.prefetchQuery({
+        queryKey: ['group-channels'],
+        queryFn: () => onGetGroupChannels(params.groupId)
+    })
+    // group subscriptions
+    await query.prefetchQuery({
+        queryKey: ['group-subscriptions'],
+        queryFn: () => onGetGroupSubscriptions(params.groupId)
+    })
+    //  member-chats
+    await query.prefetchQuery({
+        queryKey: ['member-chats'],
+        queryFn: () => onGetAllGroupMembers(params.groupId)
+    })
+
+    return <div>GroupLayout</div>
+}
+
+export default GroupLayout
