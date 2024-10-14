5a6aea113f52e08d50cba538af3b46d8760d6fa6
Ian-Chege <ianchegedev@gmail.com>
2024-10-14 16:00:42 +0300

    feat: Add functions for retrieving group information
    - Add onGetGroupInfo function to retrieve information about a specific group
    - Add onGetUserGroups function to retrieve a user's groups and memberships
    - Add onGetGroupChannels function to retrieve channels for a specific group
    - Add onGetGroupSubscriptions function to retrieve subscriptions for a specific group
    - Add onGetAllGroupMembers function to retrieve all members of a specific group

--- src/actions/groups.ts
+++ src/actions/groups.ts
@@ -4,6 +4,7 @@ import { CreateGroupSchema } from "@/components/forms/create-group/schema"
 import { client } from "@/lib/prisma"
 import { v4 as uuidv4 } from "uuid"
 import { z } from "zod"
+import { onAuthenticatedUser } from "./auth"
 
 export const onCreateNewGroup = async (
   userId: string,
@@ -106,5 +107,165 @@ export const onGetAffiliateInfo = async (id: string) => {
     return { status: 404 }
   } catch (error) {
     return { status: 400 }
+  }
+}
+
+export const onGetGroupInfo = async (groupId: string) => {
+  try {
+    const user = await onAuthenticatedUser()
+    const group = await client.group.findUnique({
+      where: {
+        id: groupId,
+      }
+    })
+
+    if (group) {
+      return {
+        status: 200,
+        group,
+        groupOwner: user.id === group.userId ? true : false,
+      }
+    }
+  } catch (error) {
+    return { status: 400 }
+  }
+}
+
+export const onGetUserGroups = async (id: string) => {
+  try {
+    const groups = await client.user.findUnique({
+      where: {
+        id,
+      },
+      select: {
+        group: {
+          select: {
+            id: true,
+            name: true,
+            icon: true,
+            channel: {
+              where: {
+                name: "general",
+              },
+              select: {
+                id: true,
+              },
+            },
+          },
+        },
+        membership: {
+          select: {
+            Group: {
+              select: {
+                id: true,
+                icon: true,
+                name: true,
+                channel: {
+                  where: {
+                    name: "general",
+                  },
+                  select: {
+                    id: true,
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    })
+
+    if (groups && (groups.group.length > 0 || groups.membership.length > 0)) {
+      return {
+        status: 200,
+        groups: groups.group,
+        members: groups.membership,
+      }
+    }
+
+    return {
+      status: 404,
+    }
+  } catch (error) {
+    return { status: 400 }
+  }
+}
+
+export const onGetGroupChannels = async (groupId: string) => {
+  try {
+    const channels = await client.channel.findMany({
+      where: {
+        groupId,
+      },
+      orderBy: {
+        createdAt: "asc",
+      }
+    })
+
+    if (channels) {
+      return {
+        status: 200,
+        channels,
+      }
+    }
+
+  } catch (error) {
+    return { status: 400, message: "Oops! Something went wrong" }
+  }
+}
+
+export const onGetGroupSubscriptions = async (groupId: string) => {
+  try {
+    const subscriptions = await client.subscription.findMany({
+      where: {
+        groupId,
+      },
+      orderBy: {
+        createdAt: "asc",
+      }
+    })
+
+    const count = await client.members.count({
+      where: {
+        groupId,
+      }
+    })
+
+    if (subscriptions) {
+      return {
+        status: 200,
+        subscriptions,
+        count,
+      }
+    }
+
+  } catch (error) {
+    return { status: 400 }
+  }
+}
+
+export const onGetAllGroupMembers = async (groupId: string) => {
+  try {
+    const user = await onAuthenticatedUser()
+    const members = await client.members.findMany({
+      where: {
+        groupId,
+        NOT: {
+          userId: user.id,
+        },
+      },
+      include: {
+        User: true,
+      }
+    })
+
+    if (members && members.length > 0) {
+      return {
+        status: 200,
+        members,
+      }
+    }
+  } catch (error) {
+    return { status: 400, message: "Oops! Something went wrong" }
   }
 }
